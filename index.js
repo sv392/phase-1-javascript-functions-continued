f2877f980c3d405db8cbe186459fd6b7b1bc6ec6
Sanh Vu <sanhvu020@gmail.com>
2022-09-07 22:41:24 -0400

    commit tree 533455abee74cf400f08da06b7dd3dfeb5f4a457

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(visualFlair = "*") {
+    function innerFunction(adjective = 'special') {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+        return innerFunction
+    }
+
+wrapAdjective('||')("a person built different")
